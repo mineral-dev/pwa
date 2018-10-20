56e21bd7f6b928caa46d808d2f0d44fee34e9d53
Reza <Reza Praditya>
2018-10-20 10:41:49 +0700

    dev

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -31,6 +31,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     
     <!--Todo 1-->
+    <link rel="manifest" href="manifest.json"/>
 
     <meta name="description" content="Mineral PWA Lab">
     <base href="{{ url('/').'/' }}" />
@@ -50,7 +51,57 @@
     @include('partials.footer')
 
     <!--Todo 4-->
+    <script type="text/javascript">
+        navigator.serviceWorker && navigator.serviceWorker.register('./service-worker.js').then(function(registration){
+            console.log('Service Worker berhasil diinstall');
+        });
+    </script>
 
     <!--Todo 5-->
+    <script type="text/javascript">
+    //dapatkan public key and private key dari https://web-push-codelab.glitch.me/
+    const publicKey = 'BAbFX3-MzGEDi1aE_p3x1QhsgcSv8vXN6XUI9jGwDxHkZNYlDCTIzFb-xMmoeJauTH_1NDlqEzwWzIUjRW0R0hU';
+    navigator.serviceWorker && navigator.serviceWorker.ready.then(function(serviceWorkerRegistration) {  
+        serviceWorkerRegistration.pushManager.getSubscription()  
+        .then(function(subscription) {  
+            
+            // check apabila object PushSubscription sudah ada
+            if (subscription) {
+                console.info('subscription sudah ada', JSON.stringify(subscription));
+                window.subscription = subscription;
+                return;  // Subscription sudah ada
+            }
+            const applicationServerKey = urlB64ToUint8Array(publicKey);
+            serviceWorkerRegistration.pushManager.subscribe({
+                userVisibleOnly: true,
+                applicationServerKey,
+            })
+            .then(function(subscription) { 
+                console.info('Subscribed to push!', JSON.stringify(subscription));
+                window.subscription = subscription;
+            }).catch(function(err) {
+                if (Notification.permission === 'denied') {
+                    console.warn('Perimission Notifikasi ditolak');
+                } else {
+                    console.error('Gagal untuk subscribe push : ', err);
+                }
+            });
+        });
+    });
+    function urlB64ToUint8Array(base64String) {
+        const padding = '='.repeat((4 - base64String.length % 4) % 4);
+        const base64 = (base64String + padding)
+            .replace(/\-/g, '+')
+            .replace(/_/g, '/');
+        const rawData = window.atob(base64);
+        const outputArray = new Uint8Array(rawData.length);
+        for (let i = 0; i < rawData.length; ++i) {
+        outputArray[i] = rawData.charCodeAt(i);
+        }
+        return outputArray;
+    }
+</script>
+<!------------>
+
 </body>
 </html>
